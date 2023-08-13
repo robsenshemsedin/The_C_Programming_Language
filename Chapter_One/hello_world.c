ee9644d69d5105816793b54fdfa621e291ffb686
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-13 09:48:04 -0700

    Hello From Robsen

---
+++ Chapter_One/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print hello world followed by new line using escape sequence.
+ *
+ * Return: Return success.
+ */
+int main(void)
+{
+	printf("Hello world!\n");
+	return (0);
+}
