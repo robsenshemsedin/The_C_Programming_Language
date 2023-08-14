bfa8894a2af5f33da29a83cf7e545c6bea0e9958
robsenshemsedin <shemsedinrobsen@gmail.com>
2023-08-13 21:20:32 -0700

    Using symbolic constants

--- Chapter_One/fahrenheit_to_celsius.c
+++ Chapter_One/fahrenheit_to_celsius.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define DECRIMENT 30
+#define MAXVALUE 300
+#define LOWER 0
 /**
  * main - Convert fahrenheit to celsius using for loop and float value.
  *
@@ -9,7 +12,7 @@ int main(void)
 {
 	int farh;
 
-	for (farh = 300; farh >= 0; farh -= 30)
+	for (farh = MAXVALUE; farh >= LOWER; farh -= DECRIMENT)
 		printf("%3d\t%4.3f\n", farh, 5.0 / 9.0 * (farh - 32));
 	return (0);
 }
